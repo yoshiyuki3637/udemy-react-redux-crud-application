1d170ecad28e2e91a48ae98bb25ba794e187f2b7
yoshiyuki <familysugar3132@icloud.com>
2021-05-31 22:21:35 +0900

    test jsx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>Hello World!</div>
-      </header>
-    </div>
+      <h1>Hello World!</h1>
   );
 }
 
