f1a0d741f5531452f7deaf532d30df6c782aac12
yoshiyuki <familysugar3132@icloud.com>
2021-06-05 19:28:35 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import './index.css';
-import App from './App';
+import recucer from './reducers'
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import reducers from './reducers';
+
+const store = createStore(reducers)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
