c7d29456cd16a0c1df5d341b6dd388298410e09d
yoshiyuki <familysugar3132@icloud.com>
2021-07-25 00:16:41 +0900

    implement event new component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENTS = 'CREATE_EVENTS'
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
@@ -7,6 +8,11 @@ const QUERYSTRING = '?token=token123'
 export const readEvents = () => async dispatch => {
     const responce = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
     dispatch ({type: READ_EVENTS, responce})
+}
+
+export const postEvents = values => async dispatch => {
+    const responce = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
+    dispatch ({type: CREATE_EVENTS, responce})
 }
 
 
--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,19 +1,56 @@
 // import { render } from '@testing-library/react'
 import React, {Component}from 'react';
 import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
 
-// import { postEvents } from '../actions';
+import { postEvents } from '../actions';
 
 class EventsNew extends Component {
-  render(){
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+  renderField(Field){
+    const { input, label, type, meta: {touched, error }} = Field
     return (
-      <React.Fragment>
-        <div>foo</div>
-      </React.Fragment>
+    <div>
+      <input {...input} placeholder={label} type={type} />
+      {touched && error && <span>{error}</span>}
+    </div>
+    )
+  }
+
+  async onSubmit(values){
+    await this.props.postEvents(values)
+    this.props.history.push('/')
+  }
+
+  render(){
+    const { handleSubmit} = this.props
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
+        <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
+        <div>
+          <input type="submit" value="Submit" disabled={false} />
+          <Link to = "/">Cancel</Link>
+        </div>
+      </form>
     )
   }
 }
-// const mapDispatchToProps = ({postEvents})
+const validate = values => {
+  const errors = {}
 
-export default connect(null, null)(EventsNew)
+  if(!values.title) errors.title = "Enter a title, please."
+  if(!values.body) errors.body = "Enter a body, please."
+
+  return errors
+}
+
+const mapDispatchToProps = ({postEvents})
+
+export default connect(null, mapDispatchToProps)(
+  reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+)
